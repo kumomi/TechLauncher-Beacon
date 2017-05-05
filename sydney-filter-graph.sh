378e91982aa1814c302248806619105bcfb20841
Andy <and_y@foxmail.com>
2017-05-05 17:34:24 +1000

    Update sydney-filter-graph.sh

--- sydney-filter-graph.sh
+++ sydney-filter-graph.sh
@@ -2,16 +2,18 @@ echo "Installation Gephi-Filter"
 git clone https://github.com/ukiyo-e/SydneyJenkins.git
 
 echo "Cd target forlder"
-cd SydneyJenkins/src/main
+cd SydneyJenkins/src/main/resources
 
 echo "Download Sydney Graphml file"
 aws s3 --region us-west-2 cp "s3://graphml.rd-switchboard/sydney/sydney.zip" .
 #sudo curl "https://s3-us-west-2.amazonaws.com/graphml.rd-switchboard/sydney/sydney.zip"
 sudo unzip sydney.zip
 
-#echo "Excute Java"
-#javac Main.java java Main
+echo "Excute Java"
+cd ../SydneyJenkins
+mvn clean install
+java -jar -Dapple.awt.UIElement="true" target/GephiFilter-1.0.jar -h 
 
-#echo "Copy files to S3..."
-#NOW="$(date +'%Y-%m-%d')"
-#aws s3 --region us-west-2 ../test2.gexf s3://graphml.rd-switchboard/sydney/filtergraph/$NOW/
+echo "Copy files to S3..."
+NOW="$(date +'%Y-%m-%d')"
+aws s3 --region us-west-2 ../SydneyJenkins/output.gexf s3://graphml.rd-switchboard/sydney/filtergraph/$NOW/
