ff9fae0b3c5c3ddaa5bae601156f92ff2403908f
Andy <and_y@foxmail.com>
2017-05-05 16:58:20 +1000

    Update sydney-filter-graph.sh

--- sydney-filter-graph.sh
+++ sydney-filter-graph.sh
@@ -5,13 +5,13 @@ echo "Cd target forlder"
 cd SydneyJenkins/src/main
 
 echo "Download Sydney Graphml file"
-aws s3 --region us-west-2 cp "s3://raphml.rd-switchboard/sydney/sydney.zip" .
+aws s3 --region us-west-2 cp "s3://graphml.rd-switchboard/sydney/sydney.zip" .
 #sudo curl "https://s3-us-west-2.amazonaws.com/graphml.rd-switchboard/sydney/sydney.zip"
 sudo unzip sydney.zip
 
-echo "Excute Java"
-javac Main.java java Main
+#echo "Excute Java"
+#javac Main.java java Main
 
-echo "Copy files to S3..."
-NOW="$(date +'%Y-%m-%d')"
-aws s3 --region us-west-2 ../test2.gexf s3://graphml.rd-switchboard/sydney/filtergraph/$NOW/
+#echo "Copy files to S3..."
+#NOW="$(date +'%Y-%m-%d')"
+#aws s3 --region us-west-2 ../test2.gexf s3://graphml.rd-switchboard/sydney/filtergraph/$NOW/
