075f26dd28d08f43b3c978d8e58df7fb81291c6a
Andy <and_y@foxmail.com>
2017-05-05 16:46:44 +1000

    Update sydney-filter-graph.sh

--- sydney-filter-graph.sh
+++ sydney-filter-graph.sh
@@ -2,16 +2,15 @@ echo "Installation Gephi-Filter"
 git clone https://github.com/ukiyo-e/SydneyJenkins.git
 
 echo "Cd target forlder"
-cd SydneyJenkins/src/
+cd SydneyJenkins/src/main
 
 echo "Download Sydney Graphml file"
-touch status
 aws s3 --region us-west-2 cp "s3://raphml.rd-switchboard/sydney/sydney.zip" .
 #sudo curl "https://s3-us-west-2.amazonaws.com/graphml.rd-switchboard/sydney/sydney.zip"
 sudo unzip sydney.zip
 
 echo "Excute Java"
-javac HeadlessSimple.java java HeadlessSimple
+javac Main.java java Main
 
 echo "Copy files to S3..."
 NOW="$(date +'%Y-%m-%d')"
