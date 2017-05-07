277fe55ce2bd244e8dc28fea65111cf466c58b1b
Andy <and_y@foxmail.com>
2017-05-07 17:30:31 +1000

    Update sydney-filter-graph.sh

--- sydney-filter-graph.sh
+++ sydney-filter-graph.sh
@@ -8,6 +8,7 @@ echo "Download Sydney Graphml file"
 aws s3 --region us-west-2 cp "s3://graphml.rd-switchboard/sydney/sydney.zip" .
 #sudo curl "https://s3-us-west-2.amazonaws.com/graphml.rd-switchboard/sydney/sydney.zip"
 sudo unzip sydney.zip
+rm sydney.zip
 
 echo "Excute Java"
 cd ..
